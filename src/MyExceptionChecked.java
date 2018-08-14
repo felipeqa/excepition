b3ec782939031c08d0ad98a075ad3467b1208dcf
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-08-13 22:58:28 -0300

    add comment

--- src/MyExceptionChecked.java
+++ src/MyExceptionChecked.java
@@ -1,3 +1,5 @@
+//Exceção do tipo Checked pois estende da classe Exception
+
 public class MyExceptionChecked extends Exception {
 
     public MyExceptionChecked(String msg){
--- src/MyExceptionUncheked.java
+++ src/MyExceptionUncheked.java
@@ -1,3 +1,4 @@
+//Exceção do tipo Unchecked pois estende da classe RuntimeException
 public class MyExceptionUncheked extends RuntimeException {
 
     public MyExceptionUncheked(String msg){
